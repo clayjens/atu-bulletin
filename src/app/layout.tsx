15c123afa38b7398c65307f58ef136bb6067b663
Clayton Jensen <clayton@yourmommas.house>
2024-09-25 11:01:54 -0500

    add opengraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,10 @@ export default function RootLayout({
             rel="icon"
             href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📌</text></svg>"
           />
+          <meta
+            property="og:image"
+            content="https://the-placeholder.vercel.app/api/og"
+          />
         </head>
         <body
           className={cn(
